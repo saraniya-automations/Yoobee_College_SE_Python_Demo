ba0f0bbd8253f0633b6ab3d9bbb197c4e866b546
saraniya-automation <saraajaysaisha@gmail.com>
2025-04-27 02:55:27 +0530

    using class for factorial example

--- Week3/Factorial.py
+++ Week3/Factorial.py
@@ -1,10 +1,29 @@
-x = input('Input a number to find factorial: ')
-x = int(x)
+class factorialDemo:
 
-if x >= 0:
-    factorial = 1  
-    for i in range(1, x + 1):
-        factorial *= i
-    print('Factorial of', x, 'is:', factorial)
-else:
-    print('Unable to find the factorial of a negative number.')
+    def __init__(self):
+        self.x = 0
+
+    def getInput(self):
+        self.x = input('Input a number to find factorial: ')
+        self.x = int(self.x)
+
+    def findFactorial(self):
+        if self.x >= 0:
+            factorial = 1  
+            for i in range(1, self.x + 1):
+                factorial *= i
+            self.printValidFactorial(self.x,factorial)
+        else:
+            self.printInvalidFactorial()
+
+    def printInvalidFactorial(self):
+        print('Unable to find the factorial of a negative number.')
+
+    def printValidFactorial(self,x,factorial):
+        print('Factorial of', x, 'is:', factorial)
+
+
+
+demo = factorialDemo()
+demo.getInput()
+demo.findFactorial()
