8ee08600e9c27d89fceb2af5721eff2a6da98610
saraniya-automation <saraajaysaisha@gmail.com>
2025-05-31 03:53:29 +0530

    additional commits

--- Week3/AreaExerciseUsingPy.py
+++
@@ -1,9 +0,0 @@
-
-
-rectangular_length = abs(10.11)
-rectangular_width = abs(11.12)
-
-area = rectangular_length * rectangular_width
-
-print('Area of rectangular land is :',area)
-print('Area of rectangular land apx is :',round(area,2))
