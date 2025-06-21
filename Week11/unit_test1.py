6073dd1926747bd87251f3fa489e5e935d7adeba
saraniya-automation <saraajaysaisha@gmail.com>
2025-06-22 03:05:26 +0530

    Completed Activity 1

---
+++ Week11/unit_test1.py
@@ -0,0 +1,17 @@
+import unittest
+
+# Unit testing is a way to test individual components of a program to ensure they work as expected. 
+# It helps to detect bugs early in the development process and ensures that changes to the code do 
+# not break existing functionality.
+
+def add(x,y):
+    return x+y
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+       
